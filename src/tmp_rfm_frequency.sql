1c53d9da00d6f09d75c9d65f2da6acacf22e849e
beslankumykov <87646293+beslankumykov@users.noreply.github.com>
2022-05-30 22:53:10 +0300

    Update tmp_rfm_frequency.sql

--- src/tmp_rfm_frequency.sql
+++ src/tmp_rfm_frequency.sql
@@ -1,5 +1,5 @@
 select u.id user_id, ntile(5) OVER(ORDER BY count(o.order_id) nulls first) frequency
 from analysis.users u
-left join orders o on u.id = o.user_id 
+left join analysis.orders o on u.id = o.user_id 
 and status = (select id from analysis.orderstatuses os where key = 'Closed') 
 group by u.id;
