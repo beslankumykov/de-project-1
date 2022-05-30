923ad2ddec9debc255643ecac019d53268655a73
beslankumykov <87646293+beslankumykov@users.noreply.github.com>
2022-05-30 18:51:07 +0300

    Update and rename test.sql to views.sql

---
+++ src/views.sql
@@ -0,0 +1,6 @@
+create view analysis.orders as select * from production.orders o;
+create view analysis.orderitems as select * from production.orderitems oi;
+create view analysis.orderstatuses as select * from production.orderstatuses os;
+create view analysis.orderstatuslog as select * from production.orderstatuslog osl;
+create view analysis.products as select * from production.products p;
+create view analysis.users as select * from production.users u;
