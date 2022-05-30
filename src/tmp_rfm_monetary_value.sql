217fb675328b9757d5ab97b208636a02cd7a5230
beslankumykov <87646293+beslankumykov@users.noreply.github.com>
2022-05-30 22:52:06 +0300

    Update tmp_rfm_monetary_value.sql

--- src/tmp_rfm_monetary_value.sql
+++ src/tmp_rfm_monetary_value.sql
@@ -1,4 +1,5 @@
-CREATE TABLE analysis.tmp_rfm_monetary_value (
- user_id INT NOT NULL PRIMARY KEY,
- monetary_value INT NOT NULL CHECK(monetary_value >= 1 AND monetary_value <= 5)
-);
+select u.id user_id, ntile(5) OVER(ORDER BY sum(o.payment) nulls first) monetary_value
+from analysis.users u
+left join orders o on u.id = o.user_id 
+and status = (select id from analysis.orderstatuses os where key = 'Closed') 
+group by u.id;
