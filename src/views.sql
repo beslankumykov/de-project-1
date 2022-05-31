12a29dd9996b5d19717cbac460d25e5b48966e29
beslankumykov <87646293+beslankumykov@users.noreply.github.com>
2022-05-31 14:23:22 +0300

    Update views.sql

--- src/views.sql
+++ src/views.sql
@@ -1,7 +1,7 @@
 -- Создание представлений в схеме analysis
-create view analysis.orders as select * from production.orders o;
-create view analysis.orderitems as select * from production.orderitems oi;
-create view analysis.orderstatuses as select * from production.orderstatuses os;
-create view analysis.orderstatuslog as select * from production.orderstatuslog osl;
-create view analysis.products as select * from production.products p;
-create view analysis.users as select * from production.users u;
+create or replace view analysis.orders as select * from production.orders o;
+create or replace view analysis.orderitems as select * from production.orderitems oi;
+create or replace view analysis.orderstatuses as select * from production.orderstatuses os;
+create or replace view analysis.orderstatuslog as select * from production.orderstatuslog osl;
+create or replace view analysis.products as select * from production.products p;
+create or replace view analysis.users as select * from production.users u;
