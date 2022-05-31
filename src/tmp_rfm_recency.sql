bcb76a6139c5c028dc572f17eda6a32871651713
beslankumykov <87646293+beslankumykov@users.noreply.github.com>
2022-05-31 14:21:09 +0300

    Update tmp_rfm_recency.sql

--- src/tmp_rfm_recency.sql
+++ src/tmp_rfm_recency.sql
@@ -2,5 +2,6 @@ insert into analysis.tmp_rfm_recency (user_id, recency)
 select u.id user_id, ntile(5) OVER(ORDER BY max(o.order_ts) nulls first) recency
 from analysis.users u
 left join analysis.orders o on u.id = o.user_id 
-and status = (select id from analysis.orderstatuses os where key = 'Closed') 
+and o.status = (select id from analysis.orderstatuses os where key = 'Closed') 
+and o.order_ts >= to_date('01.01.2021','dd.mm.yyyy')
 group by u.id;
