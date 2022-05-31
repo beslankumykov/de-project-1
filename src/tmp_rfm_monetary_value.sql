dea5d07fe4b8d69c6990cdfa7a35a5c389391e3b
beslankumykov <87646293+beslankumykov@users.noreply.github.com>
2022-05-31 14:21:36 +0300

    Update tmp_rfm_monetary_value.sql

--- src/tmp_rfm_monetary_value.sql
+++ src/tmp_rfm_monetary_value.sql
@@ -2,5 +2,6 @@ insert into analysis.tmp_rfm_monetary_value (user_id, monetary_value)
 select u.id user_id, ntile(5) OVER(ORDER BY sum(o.payment) nulls first) monetary_value
 from analysis.users u
 left join analysis.orders o on u.id = o.user_id 
-and status = (select id from analysis.orderstatuses os where key = 'Closed') 
+and o.status = (select id from analysis.orderstatuses os where key = 'Closed') 
+and o.order_ts >= to_date('01.01.2021','dd.mm.yyyy')
 group by u.id;
